5d7377950918c53ac1a912d1fe0b4c739b326d5a
Pearl <beincidemirag@gmail.com>
2022-03-31 15:49:03 +0300

    31.03.2022 ders sonu

---
+++ DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
@@ -0,0 +1,38 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCategoryDal : ICategoryDal
+    {
+        public void Add(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAllByCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -27,6 +28,11 @@ namespace DataAccess.Concrete.InMemory
             _products.Add(product);
         }
 
+        public void Add(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
         public void Delete(Product product)
         {
             //LINQ : Language Integrated Query
@@ -34,9 +40,34 @@ namespace DataAccess.Concrete.InMemory
             _products.Remove(productToDelete);
         }
 
+        public void Delete(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category Get(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Product> GetAll()
         {
             return _products;
+        }
+
+        public List<Category> GetAll(Expression<Func<Category, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
         }
 
         public List<Product> GetAllByCategory(int categoryId)
@@ -52,6 +83,11 @@ namespace DataAccess.Concrete.InMemory
             productToUpdate.CategoryId = product.CategoryId;
             productToUpdate.UnitPrice = product.UnitPrice;
             productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+
+        public void Update(Category entity)
+        {
+            throw new NotImplementedException();
         }
     }
 }
